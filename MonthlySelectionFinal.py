2f0525433a89974b507ee997194212c45c5926e7
Sai <saicharan9514@gmail.com>
2020-03-28 09:32:08 +0100

    changes to monthly logic

--- MonthlySelectionFinal.py
+++ MonthlySelectionFinal.py
@@ -10,39 +10,60 @@ valueTime = 0.003231
 eScooterLimit = 3600
 EScooterCost = 0.0033
 MonthlyPackageCost = 96
+waitingTimeValue = 600 * valueTime
+
 
 directory = "/home/sai/PycharmProjects/BerlinRoutes/OutputsGurobi/Students/User1/"
 
+# getting monthly user trips
 user_monthly_trips = getData.getUserMonthlyTripDetails()
-
+print(user_monthly_trips)
+# creating the model for monthly package route selection
 m = Model('MonthlyPackageRoutes')
+
+# creating monthly over usage variable for e scooter limit
 MonthlyOverUsage = m.addVar(vtype=GRB.INTEGER, name='MonthlyOverUsage')
+
 r = {}
+transfer = {}
+
 edgeAttrs = {}
 publicEdgeAttrs = {}
 scootEdgeAttrs = {}
 edgeIn = {}
 edgeOut = {}
+edgeModeIn = {}
+edgeModeOut = {}
+
 subNodesList = []
 monValTime = {}
 eScooterTime = {}
 tripNum = 0
 
+# Get list of Public transport modes from list of public transport modes
+publicModeList = []
+with open('publicModeList.csv', 'r') as csv_file:
+    ff = csv.reader(csv_file)
+    for eachRow in ff:
+        publicModeList.append(eachRow[0])
+
+# iterating through the trips one by one
 for trip in user_monthly_trips:
+    print(tripNum)
+    # getting origin and destination for each trip
     orig = trip[0]
     dest = trip[1]
 
     # getting the network file with public transport and e scooters
-    G = nx.read_gpickle("/home/sai/PycharmProjects/BerlinRoutes/OutputGraphs/networkBerlin1.gpickle")
-    # nx.draw_networkx(G, with_labels=True, node_size=10, font_size=2, arrowsize=4)
-    # plt.savefig("/home/sai/PycharmProjects/BerlinRoutes/OutputGraphs/sample_" + str(week) + "_" + str(tripNum) + ".pdf", bbox_inches='tight', format='pdf', dpi=1200)
+    G = nx.read_gpickle("/home/sai/PycharmProjects/BerlinRoutes/OutputGraphs/networkBerlinNew.gpickle")
+
+    # nx.draw_networkx(G, with_labels=True, node_size=10, font_size=2, arrowsize=4) plt.savefig(
+    # "/home/sai/PycharmProjects/BerlinRoutes/OutputGraphs/sample_" + str(week) + "_" + str(tripNum) + ".pdf",
+    # bbox_inches='tight', format='pdf', dpi=1200)
 
     node_attrs = nx.get_node_attributes(G, name='locale')
-    print(*node_attrs)
-    print("No. of initial edges ", nx.number_of_edges(G))
 
     # adding links between orig or dest to stops or e scooters
-
     for item in node_attrs.items():
         d1 = geopy.distance.vincenty(orig, item[1]).km
         d2 = geopy.distance.vincenty(dest, item[1]).km
@@ -56,32 +77,57 @@ for trip in user_monthly_trips:
                 G.add_edge("orig", item[0], key='walk', attrs={"walk": int(720 * d1)})
             if d2 < 5:
                 G.add_edge(item[0], "dest", key='scoot', attrs={"scoot": int(180 * d2)})
-    print("No. of edges after orig, dest and e scooters: ", nx.number_of_edges(G))
+    print("Total No. of edges after connecting orig, dest and e scooters: ", nx.number_of_edges(G))
     edgeList = G.edges.data('attrs')
-    for eachEdge in edgeList:
-        edgeAttrs[tripNum, (eachEdge[0], eachEdge[1])] = eachEdge[2]
-        if 'walk' not in eachEdge[2].keys():
-            if 'scoot' not in eachEdge[2].keys():
-                publicEdgeAttrs[tripNum, (eachEdge[0], eachEdge[1])] = eachEdge[2]
-            else:
-                scootEdgeAttrs[tripNum, (eachEdge[0], eachEdge[1])] = eachEdge[2]
 
+    # Creating separate dictionaries of public transport edges, Escooter edges and all edges
+    for eachEdge in edgeList:
+        for modeKey in eachEdge[2]:
+            edgeAttrs[tripNum, (eachEdge[0], eachEdge[1], modeKey)] = eachEdge[2]
+            if 'walk' != modeKey:
+                if 'scoot' != modeKey:
+                    # Public transport edges for each trip
+                    publicEdgeAttrs[tripNum, (eachEdge[0], eachEdge[1], modeKey)] = eachEdge[2]
+                else:
+                    # EScooter edges for each trip
+                    scootEdgeAttrs[tripNum, (eachEdge[0], eachEdge[1], modeKey)] = eachEdge[2]
+
+    # looping through all edges one by one to create dict of "in" edges and "out" edges from a node
+    # for the dicts edgeIn and edgeOut - key: (trip, nodes), value: list of edges entering/leaving the vertex
+    # adding a binary variable for each link
     for edges in edgeAttrs:
+        # condition to only create these dictionaries for the current trip (to avoid duplicates)
         if edges[0] == tripNum:
             edgeLink = (edges[1][0], edges[1][1])
-            u = edges[1][0]
-            v = edges[1][1]
-            r[tripNum, edgeLink] = m.addVar(vtype=GRB.BINARY, name='route_' + str(tripNum) + str(edgeLink))
-            if (tripNum, v) in edgeIn:
-                edgeIn[tripNum, v].append(r[tripNum, edgeLink])
-            else:
-                edgeIn[tripNum, v] = [r[tripNum, edgeLink]]
-            if (tripNum, u) in edgeOut:
-                edgeOut[tripNum, u].append(r[tripNum, edgeLink])
-            else:
-                edgeOut[tripNum, u] = [r[tripNum, edgeLink]]
+            u = edges[1][0]     # from node
+            v = edges[1][1]     # to node
 
+            # route decision variable based on tripNum, and edges
+            r[edges] = m.addVar(vtype=GRB.BINARY, name='route_' + str(edges))
+
+            if (tripNum, v) in edgeIn:
+                edgeIn[tripNum, v].append(r[edges])
+            else:
+                edgeIn[tripNum, v] = [r[edges]]
+            if (tripNum, u) in edgeOut:
+                edgeOut[tripNum, u].append(r[edges])
+            else:
+                edgeOut[tripNum, u] = [r[edges]]
+
+            # edge In and Out dictionaries along with their mode - used for transfer constraint
+            if (tripNum, v, edgeLink[1][2]) in edgeModeIn:
+                edgeModeIn[tripNum, v, edgeLink[1][2]].append(r[edges])
+            else:
+                edgeModeIn[tripNum, v, edgeLink[1][2]] = [r[edges]]
+            if (tripNum, u, edgeLink[1][2]) in edgeModeOut:
+                edgeModeOut[tripNum, u, edgeLink[1][2]].append(r[edges])
+            else:
+                edgeModeOut[tripNum, u, edgeLink[1][2]] = [r[edges]]
+
+    # list of nodes in each trip
     subNodesList.append(list(G.nodes()))
+
+    """
     # write the timings in csv file for viewing
     with open(directory + 'Timings/' + 'Timings_trip_' + str(tripNum) + '.csv', 'w') as csv_file:
         writer = csv.writer(csv_file)
@@ -90,18 +136,21 @@ for trip in user_monthly_trips:
             if edgeAttr[0] == tripNum:
                 writer.writerow([str.encode(str(edgeAttr[1])), int(list(edgeAttrs[edgeAttr].values())[0])])
     plt.clf()
+    """
     tripNum += 1
 
+# removing orig and dest in each list of nodes
 for nodes in subNodesList:
     nodes.remove('orig')
     nodes.remove('dest')
-    #print(*nodes)
 
 for edgeAttr in edgeAttrs:
-    monValTime[edgeAttr[0], edgeAttr[1]] = int(list(edgeAttrs[edgeAttr].values())[0]) * valueTime
+    monValTime[edgeAttr] = int(list(edgeAttrs[edgeAttr].values())[0]) * valueTime
 
 for edgeAttr in scootEdgeAttrs:
-    eScooterTime[edgeAttr[0], edgeAttr[1]] = (int(list(edgeAttrs[edgeAttr].values())[0]))
+    eScooterTime[edgeAttr] = (int(list(edgeAttrs[edgeAttr].values())[0]))
+
+# creating constraints for whole month
 
 # constraint to make sure each node is left if it is reached (flow constraint)
 for t in range(0, tripNum):
@@ -110,6 +159,18 @@ for t in range(0, tripNum):
 
     # destination constraint
     m.addConstr((quicksum(edgeIn[t, 'dest']) == 1), name='destConst')
+
+    # add transfer binary variable for each node, each mode
+    print("log for creating transfer decision variable")
+    for nodes in subNodesList[t]:
+        for mode in publicModeList:
+            if (t, nodes, mode) in edgeModeIn or (t, nodes, mode) in edgeModeOut:
+                transfer[t, nodes, mode] = m.addVar(vtype=GRB.BINARY,
+                                                    name='transfer_' + str(t) + str(nodes) + str(mode))
+            if (t, nodes, mode) not in edgeModeIn:
+                edgeModeIn[t, nodes, mode] = [0]
+            if (t, nodes, mode) not in edgeModeOut:
+                edgeModeOut[t, nodes, mode] = [0]
 
     for node in subNodesList[t]:
         # node = subNodes[i]
@@ -122,12 +183,21 @@ for t in range(0, tripNum):
         # constraint to make sure only one edge is leaving a node
         m.addConstr(quicksum(edgeOut[t, node]) <= 1, name='departConstr')
 
+        # transfer constraint at each node for each mode for each trip
+        for mode in publicModeList:
+            if (t, node, mode) in transfer:
+                m.addConstr(
+                    quicksum(edgeModeOut[t, node, mode]) - quicksum(edgeModeIn[t, node, mode]) <= transfer[
+                        t, node, mode],
+                    name='transferConstr')
+
 # Monthly OverUsage Constraint
 m.addConstr(quicksum(
-    r[edgeAttr] * eScooterTime[edgeAttr] for edgeAttr in scootEdgeAttrs.keys()) <= eScooterLimit + MonthlyOverUsage)
+    r[edgeAttr] * eScooterTime[edgeAttr] for edgeAttr in scootEdgeAttrs) <= eScooterLimit + MonthlyOverUsage)
 
 # Objective Function
-obj = quicksum(r[edgeAttr] * monValTime[edgeAttr] for edgeAttr in edgeAttrs.keys()) + \
+obj = quicksum(r[edgeAttr] * monValTime[edgeAttr] for edgeAttr in edgeAttrs) + \
+      quicksum(transfer.values()) * waitingTimeValue + \
       EScooterCost * MonthlyOverUsage + MonthlyPackageCost
 
 m.setObjective(obj, GRB.MINIMIZE)
@@ -137,7 +207,7 @@ var_values = {}
 for v in m.getVars():
     var_values[(str.encode(v.varName))] = v.x
 
-print("var_values are  ", var_values.items())
+# print("var_values are  ", var_values.items())
 
 with open(directory + 'ObjectiveValueMonthly.csv', 'w') as csv_file:
     writer = csv.writer(csv_file)
@@ -150,9 +220,9 @@ with open(directory + 'ObjectiveValueMonthly.csv', 'w') as csv_file:
             writer.writerow(["Over-Usage Cost", EScooterCost * overUsage])
     writer.writerow(["Monetary Time Value", obj.getValue() - MonthlyPackageCost - (EScooterCost * overUsage)])
 
-
 with open(directory + 'OptimumSolutionMonthly.csv', 'w') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(["Variables", "Values"])
     for key, value in var_values.items():
-        writer.writerow([key, value])
+        if value != 0:
+            writer.writerow([key, value])
--- WeeklySelectionFinalUpdated.py
+++ WeeklySelectionFinalUpdated.py
@@ -31,13 +31,14 @@ with open(directory + 'ObjectiveValuesWeekly.csv', 'w') as csv_file:
     writer.writerow(["Week", "Objective", "Values"])
 
 # iterating through the weeks one by one
-
 for week in user_weekly_trips:
     trips = user_weekly_trips[week]  # includes all the trips for this week as an array
 
     # Creating model for the entire week
 
     m = Model('WeeklyPackageRoutes')
+
+    # creating over usage constraint for e scooter and public transport for each week
     WeeklyOverUsage = m.addVar(vtype=GRB.INTEGER,
                                name='ScooterWeeklyOverUsage')  # Overusage of scooter decision variable
     publicOverUsage = m.addVar(vtype=GRB.INTEGER,
@@ -69,25 +70,24 @@ for week in user_weekly_trips:
 
     # print(week)
     for trip in trips:
-
+        # getting origin and destination for each trip in the week
         orig = trip[0]
         dest = trip[1]
-        print(tripNum)
+
+        # Decision variable to check if public transport was used in this trip
         x[tripNum] = m.addVar(vtype=GRB.BINARY, name='publicUsage_trip_' + str(
-            tripNum))  # Decision variable to check if public transport was used in this trip
+            tripNum))
 
         # getting the network file with public transport and e scooters
+        G = nx.read_gpickle("/home/sai/PycharmProjects/BerlinRoutes/OutputGraphs/networkBerlin1.gpickle")
 
-        G = nx.read_gpickle("/home/sai/PycharmProjects/BerlinRoutes/OutputGraphs/networkBerlinNew.gpickle")
-        # nx.draw_networkx(G, with_labels=True, node_size=10, font_size=2, arrowsize=4)
-        # plt.savefig("/home/sai/PycharmProjects/BerlinRoutes/OutputGraphs/sample_" + str(week) + "_" + str(tripNum) + ".pdf", bbox_inches='tight', format='pdf', dpi=1200)
+        # nx.draw_networkx(G, with_labels=True, node_size=10, font_size=2, arrowsize=4) plt.savefig(
+        # "/home/sai/PycharmProjects/BerlinRoutes/OutputGraphs/sample_" + str(week) + "_" + str(tripNum) + ".pdf",
+        # bbox_inches='tight', format='pdf', dpi=1200)
 
         node_attrs = nx.get_node_attributes(G, name='locale')
-        # print(*node_attrs)
-        # print("No. of initial edges ", nx.number_of_edges(G))
 
         # adding links between orig or dest to stops or e scooters
-
         for item in node_attrs.items():
             d1 = geopy.distance.vincenty(orig, item[1]).km
             d2 = geopy.distance.vincenty(dest, item[1]).km
@@ -101,7 +101,7 @@ for week in user_weekly_trips:
                     G.add_edge("orig", item[0], key='walk', attrs={"walk": int(720 * d1)})
                 if d2 < 5:
                     G.add_edge(item[0], "dest", key='scoot', attrs={"scoot": int(180 * d2)})
-        print("Total No. of edges after orig, dest and e scooters: ", nx.number_of_edges(G))
+        print("Total No. of edges after connecting orig, dest and e scooters: ", nx.number_of_edges(G))
         edgeList = G.edges.data('attrs')
 
         # Creating separate dictionaries of public transport edges, Escooter edges and all edges
@@ -111,13 +111,15 @@ for week in user_weekly_trips:
                     2]  # All edges of the trip in dictionary
                 if 'walk' != modeKey:
                     if 'scoot' != modeKey:
+                        # Public transport edges for each trip
                         publicEdgeAttrs[tripNum, (eachEdge[0], eachEdge[1], modeKey)] = eachEdge[
-                            2]  # Public transport edges for each trip
+                            2]
                     else:
+                        # EScooter edges for each trip
                         scootEdgeAttrs[tripNum, (eachEdge[0], eachEdge[1], modeKey)] = eachEdge[
-                            2]  # EScooter edges for each trip
+                            2]
 
-        # looping through all edges one by one to create dict of "in" edges and "out" edges from a node
+                        # looping through all edges one by one to create dict of "in" edges and "out" edges from a node
         # for the dicts edgeIn and edgeOut - key: (trip, nodes), value: list of edges entering/leaving the vertex
         # adding a binary variable for each link
         for edges in edgeAttrs:
@@ -126,8 +128,9 @@ for week in user_weekly_trips:
                 edgeLink = (edges[1][0], edges[1][1])  # "from node" to "to node"
                 u = edges[1][0]  # from node
                 v = edges[1][1]  # to node
-                r[edges] = m.addVar(vtype=GRB.BINARY, name='r_' + str(tripNum) + str(
-                    edges))  # route decision variable based on tripNum, and edges
+
+                # route decision variable based on tripNum, and edges
+                r[edges] = m.addVar(vtype=GRB.BINARY, name='route_' + str(edges))
 
                 if (tripNum, v) in edgeIn:
                     edgeIn[tripNum, v].append(r[edges])
@@ -148,25 +151,26 @@ for week in user_weekly_trips:
                 else:
                     edgeModeOut[tripNum, u, edgeLink[1][2]] = [r[edges]]
 
-            subNodesList.append(list(G.nodes()))  # list of nodes in each trip
+        # list of nodes in each trip
+        subNodesList.append(list(G.nodes()))
 
-            """
-            write the timings in csv file for viewing
-            with open(directory + week + '_timings_trip_' + str(
-                    tripNum) + '.csv', 'w') as csv_file:
-                writer = csv.writer(csv_file)
-                writer.writerow(["edges", "timings"])
-                for edgeAttr in edgeAttrs:
-                    if edgeAttr[0] == tripNum:
-                        writer.writerow([str.encode(str(edgeAttr[1])), int(list(edgeAttrs[edgeAttr].values())[0])])
-            plt.clf()
-            """
+        """
+        write the timings in csv file for viewing
+        with open(directory + week + '_timings_trip_' + str(
+                tripNum) + '.csv', 'w') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(["edges", "timings"])
+            for edgeAttr in edgeAttrs:
+                if edgeAttr[0] == tripNum:
+                    writer.writerow([str.encode(str(edgeAttr[1])), int(list(edgeAttrs[edgeAttr].values())[0])])
+        plt.clf()
+        """
         tripNum += 1
 
-    for nodes in subNodesList:  # removing orig and dest in each list of nodes
+    # removing orig and dest in each list of nodes
+    for nodes in subNodesList:
         nodes.remove('orig')
         nodes.remove('dest')
-        # print(*nodes)
 
     for edgeAttr in edgeAttrs:
         monValTime[edgeAttr] = int(list(edgeAttrs[edgeAttr].values())[0]) * valueTime
@@ -235,6 +239,7 @@ for week in user_weekly_trips:
 
     m.setObjective(obj, GRB.MINIMIZE)
     m.optimize()
+
     var_values = {}
     for v in m.getVars():
         var_values[(str.encode(v.varName))] = v.x
@@ -255,7 +260,8 @@ for week in user_weekly_trips:
                 scooterOverUsage = value
                 writer.writerow([week, "Scooter Over-Usage Cost", EScooterCost * scooterOverUsage])
         writer.writerow([week, "Monetary Time Value", obj.getValue() - WeeklyPackageCost - (EScooterCost *
-                        scooterOverUsage) - (publicTransportCost * publicTransOverUsage)])
+                                                                                            scooterOverUsage) - (
+                                     publicTransportCost * publicTransOverUsage)])
 
     i = 0
     with open(directory + 'OptimumRouteWeekly.csv', 'a') as csv_file:
